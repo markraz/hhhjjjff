e56e2fcb80c3f2e2be0382019bf194b43d49b07c
zlalalal@sina.com.cn <zlalalal@sina.com.cn@cfa3df76-2cb6-11de-b2a4-4f96ceaaac44>
2009-05-18 04:08:23 +0000

    接近发布版

--- drawcli/summinfo.cpp
+++ drawcli/summinfo.cpp
@@ -1,13 +1,4 @@
 // summinfo.cpp : implementation of the CDrawDoc class
-//
-// This is a part of the Microsoft Foundation Classes C++ library.
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//
-// This source code is only intended as a supplement to the
-// Microsoft Foundation Classes Reference and related
-// electronic documentation provided with the library.
-// See these sources for detailed information regarding the
-// Microsoft Foundation Classes product.
 
 #include "stdafx.h"
 #include <winnls.h>
